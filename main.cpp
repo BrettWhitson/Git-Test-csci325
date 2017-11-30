49ceb1b13072aaa483fc1d583adb13a85dbb98a1
Brett Whitson <bremwhit@ut.utm.edu>
2017-11-30 12:57:35 -0600

    Modified Main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
+int sum(int n);
+
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
-
+  int num;
+  
+  cout << "Hello World!" << endl;
+  cout << "Please enter a number: ";
+  cin >> num;
+  num = sum(num);
+  cout << "Sum: " << num << endl;
   return 0;
+}
+
+int sum(int n)
+{
+  int num = 0;
+  for(int i = 0; i <= n; i++)
+    {
+      num += i;
+    }
+  return num;
 }
